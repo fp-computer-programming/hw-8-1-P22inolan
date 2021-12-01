4a86c728fe1b45b36d3fd12ab3a8aa15bcb5dab9
P22inolan <p22inolan@fairfieldprep.org>
2021-12-01 10:21:48 -0500

    8-1-1 submit

--- hw8-1-1.py
+++ hw8-1-1.py
@@ -2,8 +2,16 @@
 
 
 def score(frees, twos, threes):
-    points = frees + (twos * 2) + (threes * 3)
-    return "The player scored " + points + " points in the game."
+    points = int(frees) + (int(twos) * 2) + (int(threes) * 3)
+    return points
 
 
-print(score(1, 1, 1) )
+tally1 = input("How many free throws were made? ")
+tally2 = input("How many twos were made? ")
+tally3 = input("How many threes were made? ")
+
+print(score(tally1, tally2, tally3))
+
+print(score(1, 1, 1) == 6)
+print(score(3, 6, 2) == 21)
+print(score(8, 10, 7) == 49)
