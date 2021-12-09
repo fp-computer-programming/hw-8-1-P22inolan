7ebe27f1e42bf265d7e509afc486e3b4ff389f4d
P22inolan <p22inolan@fairfieldprep.org>
2021-12-09 10:15:37 -0500

    Submit 8-1

--- hw8-1-5.py
+++ hw8-1-5.py
@@ -1,18 +1,21 @@
 # Author: IBN (AMDG) 12/6/2021
 
-def wincalc(total1, total2):
-    total1 = (win1 * 3) + (draw1 * 1)
-    total2 = (win2 * 3) + (draw2 * 1)
+def wincalc(win1, draw1, win2, draw2):
+    total1 = (int(win1) * 3) + int(draw1)
+    total2 = (int(win2)* 3) + int(draw2)
+    if total1 > total2:
+        return "Team 1 had more points. "
+    else:
+        return "Team 2 had more points. "
+
 
 win1 = input("How many wins did Team 1 have? ")
 draw1 = input("How many draws did Team 1 have? ")
 win2 = input("How many wins did Team 2 have? ")
 draw2 = input("How many draws did Team 2 have? ")
 
-total1 = (int(win1) * 3) + (int(draw1) * 1)
-total2 = (int(win2) * 3) + (int(draw2) * 1)
+wincalc(win1, draw1, win2, draw2)
 
-if total1 > total2:
-    print("Team 1 had more points.")
-else:
-    print("Team 2 had more points.")
+print(wincalc(10, 0 ,5 ,5) == "Team 1 had more points. ")
+print(wincalc(4, 5, 5, 4) == "Team 2 had more points. ")
+print(wincalc(10, 2, 8, 3) == "Team 1 had more points. ")
